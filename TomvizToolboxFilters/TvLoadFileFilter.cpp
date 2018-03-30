415320981bfb9baac8d84731b688f0e5a73ac8c2
Michael Jackson <mike.jackson@bluequartz.net>
2018-03-29 22:40:52 -0400

    CLANG-TIDY: Code cleanup based on suggestions from clang-tidy.

--- TomvizToolboxFilters/TvLoadFileFilter.cpp
+++ TomvizToolboxFilters/TvLoadFileFilter.cpp
@@ -48,7 +48,6 @@
 //
 // -----------------------------------------------------------------------------
 TvLoadFileFilter::TvLoadFileFilter()
-: AbstractFilter()
 {
   initialize();
   setupFilterParameters();
